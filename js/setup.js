286da670aefcf89cc0cc4194ee7b05cd945f22b6
Dmitriy <mi5ha6in@gmail.com>
2018-04-09 23:46:08 +0500

    Adds generate wizards array

--- js/setup.js
+++ js/setup.js
@@ -55,20 +55,48 @@ var randomInteger = function (min, max) {
 
 //  Возвращает случайное значение из массива
 var getRandomArrayData = function (array) {
-  return array[randomInteger(0, array.length - 1)]
-}
+  return array[randomInteger(0, array.length - 1)];
+};
 
 //  Возвращает случайное имя
 var getRandomName = function (arrayName) {
   return getRandomArrayData(arrayName);
-}
+};
 
 //  Возвращает случайную фамилию
 var getRandomLastName = function (arrayLastName) {
   return getRandomArrayData(arrayLastName);
-}
-
-//  Возвращает случайное полное имя
-var getRandomFullName = function (arrayName, arrayLastName) {
-  return getRandomName(arrayName) + ' ' + getRandomLastName(arrayLastName);
 };
+
+//  Возвращает случайный цвет куртки
+var getRandomCoatColor = function (arrayCoatColor) {
+  return getRandomArrayData(arrayCoatColor);
+};
+
+//  Возвращает случайный цвет глаз
+var getRandomEyesColor = function (arrayEyesColor) {
+  return getRandomArrayData(arrayEyesColor);
+};
+
+//  Возвращает случайный объект волшебника
+var getRandomWizard = function (arrayName, arrayLastName, arrayCoatColor, arrayEyesColor) {
+  var wizard = {};
+  wizard.name = getRandomName(arrayName);
+  wizard.lastName = getRandomLastName(arrayLastName);
+  wizard.coatColor = getRandomCoatColor(arrayCoatColor);
+  wizard.eyesColor = getRandomEyesColor(arrayEyesColor);
+  return wizard;
+};
+
+//  Возвращает массив случайных влошебников
+var getArrayWizards = function (arrayName, arrayLastName, arrayCoatColor, arrayEyesColor, wizardsLength) {
+  var arrayWizards = [];
+
+  for (var i = 0; i < wizardsLength; i++) {
+    arrayWizards.push(getRandomWizard(arrayName, arrayLastName, arrayCoatColor, arrayEyesColor));
+  }
+
+  return arrayWizards;
+};
+
+var arrayWizards = getArrayWizards(WIZARDS_NAME, WIZARDS_LAST_NAME, COAT_COLORS, EYES_COLORS, 4);
